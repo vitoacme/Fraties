fbda9bae3704ebd3cd5d93d9722c258da32e701f
acgilje <a.gilje@usask.ca>
2016-11-30 16:12:37 -0600

    Missed image fix

--- home.php
+++ home.php
@@ -283,22 +283,14 @@ while($row = mysqli_fetch_assoc($leaders)) {
     $imagePath = $row["userImagePath"];
     $points = $row["userPoints"];
     $name = $row["userFirstName"];
-<<<<<<< Updated upstream
     $nsid = $row["userNSID"];
     if(getUserActiveStatus($nsid)){
         echo "<li class='w3-padding-16'>";
-          echo "<img src='{$imagePath}' class='w3-left w3-circle w3-margin-right' style='width:60px'>";
+          echo "<img src='{$imagePath}' class='w3-left w3-circle w3-margin-right' style='width:60px; height:60px;'>";
           echo "<span class='w3-xlarge'>{$name}</span><br>";
           echo "<span>{$points} points</span>";
         echo "</li>";
     }
-=======
-            echo "<li class='w3-padding-16'>";
-              echo "<img src='{$imagePath}' class='w3-left w3-circle w3-margin-right' style='width:60px; height:60px;'>";
-              echo "<span class='w3-xlarge'>{$name}</span><br>";
-              echo "<span>{$points} points</span>";
-            echo "</li>";
->>>>>>> Stashed changes
 }
 ?>
           </ul>

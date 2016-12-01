6a39e69cfa95e5503c6a40948e2dd479422d2308
Vishal Tomar <vishaltomar@hotmail.ca>
2016-11-30 18:16:13 -0600

    NewStart1.1

--- Database/fratiesDatabase.sql
+++ Database/fratiesDatabase.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Nov 28, 2016 at 11:54 PM
+-- Generation Time: Dec 01, 2016 at 01:15 AM
 -- Server version: 5.6.33
 -- PHP Version: 5.6.27
 
@@ -15,6 +15,29 @@ SET time_zone = "+00:00";
 --
 CREATE DATABASE IF NOT EXISTS `fraties` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `fraties`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+DROP TABLE IF EXISTS `comments`;
+CREATE TABLE `comments` (
+  `commentID` int(254) UNSIGNED NOT NULL,
+  `postID` int(254) UNSIGNED NOT NULL,
+  `userNSID` varchar(100) NOT NULL,
+  `commentText` varchar(150) NOT NULL,
+  `commentTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`commentID`, `postID`, `userNSID`, `commentText`, `commentTime`) VALUES
+(1, 27, 'vit655', 'hey', '2016-12-01 00:09:55'),
+(2, 27, 'vit655', 'hello', '2016-12-01 00:10:07');
 
 -- --------------------------------------------------------
 
@@ -39,7 +62,7 @@ CREATE TABLE `posts` (
 --
 
 INSERT INTO `posts` (`postID`, `userNSID`, `userCollege`, `postText`, `postUpVotes`, `postDownVotes`, `postComments`, `postTime`) VALUES
-(18, 'vit655', 'Arts & Science', 'this is the first post!', 1, -1, 0, '2016-11-28 21:17:46'),
+(18, 'vit655', 'Edwards School of Business', 'this is the first post!', 1, -1, 0, '2016-12-01 00:13:09'),
 (19, 'sym123', 'Arts & Science', 'this is Symon\'s post!', 1, 0, 0, '2016-11-28 21:28:51'),
 (20, 'sym123', 'Arts & Science', 'lets move upvote points to 4', 0, 0, 0, '2016-11-28 21:59:22'),
 (21, 'sym123', 'Arts & Science', 'try again', 0, 0, 0, '2016-11-28 22:04:17'),
@@ -47,7 +70,31 @@ INSERT INTO `posts` (`postID`, `userNSID`, `userCollege`, `postText`, `postUpVot
 (23, 'sym123', 'Arts & Science', 'try again #5', 0, 0, 0, '2016-11-28 22:04:55'),
 (24, 'sym123', 'Arts & Science', 'post a post', 0, 0, 0, '2016-11-28 22:11:17'),
 (25, 'sym123', 'Arts & Science', 'increase points to 2 but not the upvote', 0, -1, 0, '2016-11-28 22:17:11'),
-(26, 'sym123', 'Arts & Science', 'increase points to 2 but not the upvote #2', 1, 0, 0, '2016-11-28 22:17:07');
+(26, 'sym123', 'Arts & Science', 'increase points to 2 but not the upvote #2', 1, 0, 0, '2016-11-28 22:17:07'),
+(27, 'vit655', 'Edwards School of Business', 'hello', 0, 0, 2, '2016-12-01 00:13:09'),
+(28, 'vit655', 'Edwards School of Business', 'blah', 0, -1, 0, '2016-12-01 00:14:32');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tags`
+--
+
+DROP TABLE IF EXISTS `tags`;
+CREATE TABLE `tags` (
+  `tagID` int(254) UNSIGNED NOT NULL,
+  `postID` int(254) UNSIGNED NOT NULL,
+  `tagText` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tags`
+--
+
+INSERT INTO `tags` (`tagID`, `postID`, `tagText`) VALUES
+(1, 27, 'usask'),
+(2, 27, 'test'),
+(3, 27, 'fml');
 
 -- --------------------------------------------------------
 
@@ -76,7 +123,7 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`userID`, `userNSID`, `userPassword`, `userFirstName`, `userLastName`, `userCollege`, `userImagePath`, `userPoints`, `userUpvotes`, `userDownvotes`, `userActive`) VALUES
 (3, 'sym123', '123', 'Symon', 'Hernandez', 'Arts & Science', 'UserImages/13690870_1010780319036979_5306694210072325842_n.jpg', 4, 0, 0, 1),
-(1, 'vit655', '123', 'Vishal', 'Tomar', 'Arts & Science', 'UserImages/mypic.jpg', 0, 0, 0, 1);
+(1, 'vit655', '123', 'Vishal', 'Tomar', 'Edwards School of Business', 'UserImages/12360281_10153291141323133_1246621340571062240_n.jpg', 3, 0, -1, 1);
 
 -- --------------------------------------------------------
 
@@ -102,44 +149,20 @@ INSERT INTO `votes` (`voteID`, `postID`, `userNSID`, `vote`, `voteTime`) VALUES
 (93, 18, 'sym123', 0, '2016-11-28 21:17:46'),
 (94, 19, 'sym123', 1, '2016-11-28 21:28:51'),
 (114, 26, 'sym123', 1, '2016-11-28 22:17:07'),
-(115, 25, 'sym123', 0, '2016-11-28 22:17:11');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `comments`
---
-
-DROP TABLE IF EXISTS `comments`;
-CREATE TABLE `comments` (
-  `commentID` int(254) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `postID` int(254) UNSIGNED NOT NULL,
-  `userNSID` varchar(100) NOT NULL,
-  `commentText` varchar(150) NOT NULL,
-  `commentTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (commentID),
-  FOREIGN KEY (postID) REFERENCES posts(postID) ON DELETE CASCADE ON UPDATE CASCADE,
-  FOREIGN KEY (userNSID) REFERENCES users(userNSID) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `comments`
---
-
-DROP TABLE IF EXISTS `tags`;
-CREATE TABLE `tags` (
-  `tagID` int(254) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `postID` int(254) UNSIGNED NOT NULL,
-  `tagText` varchar(150) NOT NULL,
-  PRIMARY KEY (tagID),
-  FOREIGN KEY (postID) REFERENCES posts(postID) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+(115, 25, 'sym123', 0, '2016-11-28 22:17:11'),
+(123, 28, 'vit655', 0, '2016-12-01 00:14:32');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`commentID`),
+  ADD KEY `postID` (`postID`),
+  ADD KEY `userNSID` (`userNSID`);
 
 --
 -- Indexes for table `posts`
@@ -147,6 +170,13 @@ CREATE TABLE `tags` (
 ALTER TABLE `posts`
   ADD PRIMARY KEY (`postID`),
   ADD KEY `userNSID` (`userNSID`);
+
+--
+-- Indexes for table `tags`
+--
+ALTER TABLE `tags`
+  ADD PRIMARY KEY (`tagID`),
+  ADD KEY `postID` (`postID`);
 
 --
 -- Indexes for table `users`
@@ -169,10 +199,20 @@ ALTER TABLE `votes`
 --
 
 --
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `commentID` int(254) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `postID` int(254) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `postID` int(254) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+--
+-- AUTO_INCREMENT for table `tags`
+--
+ALTER TABLE `tags`
+  MODIFY `tagID` int(254) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `users`
 --
@@ -182,16 +222,29 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `votes`
 --
 ALTER TABLE `votes`
-  MODIFY `voteID` int(254) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=119;
+  MODIFY `voteID` int(254) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=124;
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`postID`) REFERENCES `posts` (`postID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`userNSID`) REFERENCES `users` (`userNSID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `posts`
 --
 ALTER TABLE `posts`
   ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`userNSID`) REFERENCES `users` (`userNSID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `tags`
+--
+ALTER TABLE `tags`
+  ADD CONSTRAINT `tags_ibfk_1` FOREIGN KEY (`postID`) REFERENCES `posts` (`postID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `votes`
